5977c45b0cdc530e07b45b6473aaf4ffbd64a8e0
Uzun Demir <94790150+UzunDemir@users.noreply.github.com>
2024-09-26 10:00:18 +0300

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,15 +1,13 @@
 from openai import OpenAI
 import streamlit as st
 
-# import os
-# # API 키 설정
-# os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
-# with st.sidebar:
-#     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-#     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-#     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-#     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
-openai_api_key = st.secrets['OPENAI_API_KEY']
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
 
 st.title("💬 Chatbot")
 st.caption("🚀 A Streamlit chatbot powered by OpenAI")
